f8e3f3abde4806b55e44217fc3dfb40f2672b221
Bilal <bilal@123.com>
2022-02-17 01:11:01 +0500

    Modified changes

--- PerformActions.java
+++ PerformActions.java
@@ -1,7 +1,9 @@
 package automation;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -13,10 +15,25 @@ public class PerformActions {
 		// maximizing the window size
 		driver.get("https://dev.fashionpass.com/");
 		driver.manage().window().maximize();
-		Actions a= new Actions(driver);
-//		a.moveToElement(driver.findElement(By.xpath("//a[@href='/clothing']/label"))).build().perform();
-		a.moveToElement(driver.findElement(By.xpath("//div[@id='__next']/header/div[5]/nav/ul/li[2]"))).build().perform();
 
+		// Making object a of action class to perform multiple actions
+		Actions a = new Actions(driver);
+		
+		// this will take us to clothing and hover
+//		a.moveToElement(driver.findElement(By.xpath("//a[@href='/clothing']/label"))).build().perform();
+		
+		// You can also write above by making a WebElement
+		// for e.g
+		WebElement clothingpath = driver.findElement(By.xpath("//a[@href='/clothing']/label"));
+
+//		a.moveToElement(clothingpath).build().perform();
+//		a.moveToElement(driver.findElement(By.xpath("//div[@id='__next']/header/div[5]/nav/ul/li[2]"))).build().perform();
+
+		a.moveToElement(driver.findElement(By.xpath("//div[@class='searchResultBox']/input"))).click()
+		.keyDown(Keys.SHIFT).sendKeys("alica moto jacket").build().perform();
+		
+		// for right click
+		a.moveToElement(clothingpath).contextClick().build().perform();
 	}
 
 }
